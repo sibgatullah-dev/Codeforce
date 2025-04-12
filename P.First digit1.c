0e7606585901951081b47a1c0ba1ed0ebfe90b46
Sibgatullah <sibgatullah881@gmail.com>
2025-04-13 02:04:47 +0600

    First digit1

---
+++ P.First digit1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    int X;
+    scanf("%d",&X);
+    int digit = X/1000;
+    if (digit%2==0)
+    {
+        printf("EVEN");
+    }
+    else{
+        printf("ODD");
+    }
+    
+    return 0;
+}
