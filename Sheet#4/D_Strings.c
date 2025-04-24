45ea8aa3445b26bd74d88e7c58bdb3f94fd9c687
Sibgatullah <sibgatullah881@gmail.com>
2025-04-25 02:14:48 +0600

    sheet4

--- Sheet#4/D_Strings.c
+++ Sheet#4/D_Strings.c
@@ -4,17 +4,26 @@ int main(){
 
     char a[11];scanf("%s", a);
     char b[11];scanf("%s", b);
-    char c[22];
-
+    
     int lenA= strlen(a);
     int lenB= strlen(b);
-    strcpy(c,a);
-    strcat(c,b);
-    printf("%d",lenA);
-    printf(" ");
-    printf("%d\n",lenB);
+
+    char c[lenA+lenB+1];
+
+    for (int i = 0; i < lenA; i++)
+    {
+        c[i]=a[i];
+    }
+    for (int i = 0; i < lenB; i++)
+    {
+        c[i+lenA]=b[i];
+    }
+    c[lenA + lenB] = '\0';
+    printf("%d %d\n",lenA,lenB);
     printf("%s\n",c);
-    printf("%s",a);
-    printf(" ");
-    printf("%s\n",b);
+    char temp= a[0];
+    a[0]=b[0];
+    b[0]=temp;
+    printf("%s %s\n",a,b);
+       
 }
