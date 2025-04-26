05094685e5baee951acea5e6f321ca128039c45a
Sibgatullah <sibgatullah881@gmail.com>
2025-04-26 21:38:59 +0600

    sheet4

---
+++ Sheet#4/C_Compare1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+int main(){
+    char x[21],y[21];
+    scanf("%s %s",&x,&y);
+    int i = 0;
+    
+    while (1)
+    {
+       
+        if (x[i]=='\0' && y[i]=='\0')
+        {
+            printf("%s",x);
+            break;
+        }
+        else if (x[i]=='\0')
+        {
+            printf("%s",x);
+            break;
+        }
+        else if(y[i]=='\0')
+        {
+            printf("%s",y);
+            break;
+        }
+        else if (x[i]<y[i])
+        {
+            printf("%s",x);
+            break;
+        }
+        else if (x[i]>y[i])
+        {
+            printf("%s",y);
+            break;
+        }
+        else if(x[i]==y[i]){
+            i++;
+        }
+    }
+    
+    return 0;
+}
