d6e050a4765cad218210c47234cfff269f51c0b8
Sibgatullah <sibgatullah881@gmail.com>
2025-04-26 21:25:46 +0600

    sheet4

---
+++ Sheet#4/C_Compare.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main(){
+    char x[21],y[21];
+    scanf("%s %s",x,y);
+    int val = strcmp(x,y);
+    if (val>0)
+    {
+        printf("%s",y);
+    }
+    else if (val<0)
+    {
+        printf("%s",x);
+    }
+    else if (val==0)
+    {
+        printf("%s",x);
+    }
+    
+    return 0;
+}
